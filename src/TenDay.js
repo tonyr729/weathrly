e4a9ce11e8d0558b369d2090ea726dddddba0ab0
Tony Robertson <tonyr729@gmail.com>
2018-04-12 21:33:27 -0600

    Style 7h and 10d cards
    Styled the 7h and 10d cards, their toggle buttons, and containers.

--- src/TenDay.js
+++ src/TenDay.js
@@ -2,13 +2,15 @@ import React from 'react';
 import Card from './Card';
 
 const TenDay = (props) => {
+ 
   let tenDayArray = props.data.map(day => {
     let highLow = `${day.high} / ${day.low}`
     return <Card title={day.day} icon={day.icon} temp={highLow} />
   })
+  
 
   return (
-    <div className="ten-day">
+    <div className={props.buttonState ? "ten-day" : "hidden"}>
       {tenDayArray}
     </div>
   )
