7d01fcac575d6a6c3d6237bce63dbd472c25d948
Tony Robertson <tonyr729@gmail.com>
2018-04-14 13:29:13 -0600

    Add tests for App
    Skipped tests that are testing for state changes.

---
+++ src/Api.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const fetchApi = (location) => {
+  return (
+    fetch(`http://api.wunderground.com/api/81347f06b321e144/conditions/forecast10day/hourly10day/q/${ location }.json`)
+  )
+}
+
+export default fetchApi;
--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../App';
-import {shallow} from 'enzyme';
+import {shallow, configure} from 'enzyme';
+import { read } from 'fs';
+
+import Search from '../Search'
+import CurrentWeather from '../CurrentWeather'
+import ForecastToggle from '../ForecastToggle'
+import SevenHour from '../SevenHour'
+import TenDay from '../TenDay'
+import MockObj from '../MockObj'
+
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
@@ -10,33 +22,78 @@ it('renders without crashing', () => {
 });
 
 describe('App', () => {
+    let renderedApp;
+    beforeEach(() => {
+      renderedApp = shallow(<App />);
+    })
 
     it('should have a default state with apiData, location, sevenHourBtnClicked, tenDayBtnClicked', () => {
+      renderedApp = shallow(<App />);
+      let expected = {
+        apiData: null,
+        location: '',
+        sevenHourBtnClicked: true,
+        tenDayBtnClicked: false
+      }
+
+      expect(renderedApp.state('apiData')).toEqual(expected.apiData)
+      expect(renderedApp.state('location')).toEqual(expected.location)
+      expect(renderedApp.state('sevenHourBtnClicked')).toEqual(expected.sevenHourBtnClicked)
+      expect(renderedApp.state('tenDayBtnClicked')).toEqual(expected.tenDayBtnClicked)
 
     })
 
-    it('should change the location in state based on user input', () => {
+    it.skip('should change the location in state based on user input', () => {
+      let shallowApp = shallow(<App />)
+      let appInst = shallowApp.instance()
+      let expectation = 'denver'
+
+      console.log(appInst.state)
+
+      appInst.submitLocation(expectation);
+      
+      let waitLog = console.log(appInst.state)
+
+      setTimeout(waitLog, 7000)
+     
+      expect(renderedApp.state('location')).toEqual(expectation);
+
+    })
+
+    it.skip('should fetch Api data based on the location set in state', () => {
       
     })
 
-    it('should fetch Api data based on the location set in state', () => {
-      
-    })
-
-    it('should toggle the forecast button values in state', () => {
+    it.skip('should toggle the forecast button values in state', () => {
       
     })
 
     it('should render a welcome screen with a Search component', () => {
 
+      expect(renderedApp.find(Search).length).toEqual(1)
+
     })
 
     it('should render a main screen with Search, CurrentWeather, ForecastToggle, SevenHour, and TenDay components', () => {
+      renderedApp.setState({ apiData: MockObj});
       
+      expect(renderedApp.find(Search).length).toEqual(1)
+      expect(renderedApp.find(CurrentWeather).length).toEqual(1)
+      expect(renderedApp.find(ForecastToggle).length).toEqual(1)
+      expect(renderedApp.find(SevenHour).length).toEqual(1)
+      expect(renderedApp.find(TenDay).length).toEqual(1)
+
     })
 
     it('should only render the main screen if apiData in state is not null', () => {
-      
+      expect(renderedApp.find('h1').length).toEqual(1)
+      expect(renderedApp.find(SevenHour).length).toEqual(0)
+
+      renderedApp.setState({ apiData: MockObj});
+
+      expect(renderedApp.find('Welcome').length).toEqual(0)
+      expect(renderedApp.find(SevenHour).length).toEqual(1)
+
     })
   
 
